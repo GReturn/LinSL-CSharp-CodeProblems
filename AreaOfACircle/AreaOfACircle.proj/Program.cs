e1404b039a5dbb41e5526f1c2f557546b3a2f020
Lindrew <66846143+GReturn@users.noreply.github.com>
2021-05-23 22:52:50 +0800

    Added the "Area of a Circle" problem

---
+++ AreaOfACircle/AreaOfACircle.proj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AreaOfACircle.proj
+{
+    class Program
+    {
+        static void Main()
+        {
+            const double pi = 3.14;
+            double radius = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine(pi * Math.Pow(radius, 2));
+        }
+    }
+}
