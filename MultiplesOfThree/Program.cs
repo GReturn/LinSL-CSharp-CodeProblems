fda0b9dba2d9996e98a3ec5bd4a02674ae5eb240
Lindrew <66846143+GReturn@users.noreply.github.com>
2021-05-25 02:06:15 +0800

    Added 'Multiples of Three' problem

---
+++ MultiplesOfThree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplesOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
