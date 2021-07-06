2313bad1cbb1ec720b8a3808a5cdb9e9e3ba84c6
Lindrew <spongebobrafael@gmail.com>
2021-07-06 09:06:42 +0800

    Added boilerplate code

--- DrawingApplication/Program.cs
+++ DrawingApplication/Program.cs
@@ -6,7 +6,43 @@ namespace DrawingApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Draw pencil = new Draw();
+            Draw brush = new Brush();
+            Draw spray = new Spray();
+
+            pencil.StartDraw();
+            brush.StartDraw();
+            spray.StartDraw();
         }
+    }
+
+    /*
+    Draw => "Using pencil"
+    Brush => "Using brush"
+    Spray => "Using spray"
+    */
+
+    public interface IDraw
+    {
+        void StartDraw();
+    }
+    class Draw : IDraw
+    {
+        public virtual void StartDraw()
+        {
+            Console.WriteLine("Using pencil");
+        }
+    }
+
+    // inherit this class from the class Draw
+    class Brush
+    {
+        // implent the StartDraw() method.
+    }
+
+    // inherit this class from the class Draw
+    class Spray
+    {
+        // implent the StartDraw() method.
     }
 }
