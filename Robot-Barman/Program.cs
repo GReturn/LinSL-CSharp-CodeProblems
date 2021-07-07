0fe16b67a4ab5a7fb1ea711c87d81d3d18a36583
Lindrew <spongebobrafael@gmail.com>
2021-07-07 16:14:13 +0800

    completed "Robot-Barman" yayayyyy

--- Robot-Barman/Program.cs
+++ Robot-Barman/Program.cs
@@ -16,19 +16,19 @@ namespace SoloLearn
                 int shelves = Convert.ToInt32(Console.ReadLine());
 
                 //your code goes here
-
+                Console.WriteLine($"{drinks / shelves}");
             }
             /*
              * 1. DivideByZeroException => "At least 1 shelf"
              * 2. FormatException => "Please insert an integer"
             */
-            catch ()
+            catch (DivideByZeroException e)
             {
-
+                Console.WriteLine("At least 1 shelf");
             }
-            catch ()
+            catch (FormatException)
             {
-
+                Console.WriteLine("Please insert an integer");
             }
         }
     }
